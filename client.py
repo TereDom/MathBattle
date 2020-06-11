255e55b1577f583bb619e31b8bc1ded79ae954e6
Антон <antonovka2310@yandex.ru>
2020-06-12 00:27:03 +0300

    Добавлена первая версия калькулятора

--- client.py
+++ client.py
@@ -10,7 +10,6 @@ USER_ID = 1
 class MathBattle(QMainWindow):
     def __init__(self):
         global current_task
-
         super().__init__()
 
         uic.loadUi('data/ui/client.ui', self)
@@ -21,6 +20,63 @@ class MathBattle(QMainWindow):
         self.ButtonPrevTask.clicked.connect(self.get_prev_task)
         self.ButtonSendAnswer.clicked.connect(self.check_answer)
 
+        self.OneCalcButton.clicked.connect(self.num_operation)
+        self.TwoCalcButton.clicked.connect(self.num_operation)
+        self.ThreeCalcButton.clicked.connect(self.num_operation)
+        self.FourCalcButton.clicked.connect(self.num_operation)
+        self.FiveCalcButton.clicked.connect(self.num_operation)
+        self.SixCalcButton.clicked.connect(self.num_operation)
+        self.SevenCalcButton.clicked.connect(self.num_operation)
+        self.EightCalcButton.clicked.connect(self.num_operation)
+        self.NineCalcButton.clicked.connect(self.num_operation)
+        self.ZeroCalcButton.clicked.connect(self.num_operation)
+
+        self.PlusCalcButton.clicked.connect(self.arithmetic_operation)
+        self.MinusCalcButton.clicked.connect(self.arithmetic_operation)
+        self.MultiplyCalcButton.clicked.connect(self.arithmetic_operation)
+        self.DivideCalcButton.clicked.connect(self.arithmetic_operation)
+        self.DotCalcButton.clicked.connect(self.arithmetic_operation)
+
+        self.DelCalcButton.clicked.connect(self.special_operation)
+        self.EqualCalcButton.clicked.connect(self.special_operation)
+
+        self.calc_board = ''
+        self.labelCalc.setText(self.nice_view(self.calc_board))
+        self.number = '0'
+
+    # Калькулятор
+    def num_operation(self):
+        self.number += self.sender().text()
+        self.calc_board += self.sender().text()
+        self.labelCalc.setText(self.nice_view(self.calc_board))
+
+    def arithmetic_operation(self):
+        self.calc_board = '0' if len(self.calc_board) == 0 else self.calc_board
+        if self.sender().text() == '.' and '.' not in self.number:
+            self.number += self.sender().text()
+            self.calc_board += self.sender().text()
+        elif self.sender().text() in ['+', '-', '*', '/']:
+            if self.calc_board[-1] in ['+', '-', '*', '/']:
+                self.calc_board = self.calc_board[:-1] + self.sender().text()
+            else:
+                self.calc_board += self.sender().text()
+                self.number = '0'
+        self.labelCalc.setText(self.nice_view(self.calc_board))
+
+    def special_operation(self):
+        if self.sender().text() == '⌫':
+            self.calc_board = self.calc_board[:-1]
+            self.labelCalc.setText(self.nice_view(self.calc_board))
+        if self.sender().text() == '=':
+            try:
+                self.calc_board = str(eval(self.calc_board))
+                self.labelCalc.setText(self.calc_board)
+            except:
+                self.labelCalc.setText('Error')
+                self.calc_board = ''
+
+    def nice_view(self, string):
+        return '0' if string == '' else string
 
     def get_next_task(self):
         global task_id, current_task
@@ -29,7 +85,8 @@ class MathBattle(QMainWindow):
             task_id = 1
         else:
             task_id += 1
-        if str(task_id) in str(get(f'http://127.0.0.1:5000/api/user_information/{USER_ID}').json()['decided_tasks']).split('%'):
+        if str(task_id) in str(
+                get(f'http://127.0.0.1:5000/api/user_information/{USER_ID}').json()['decided_tasks']).split('%'):
             self.get_next_task()
         current_task = get(f'http://127.0.0.1:5000/api/get_task/{task_id}').json()
         self.post_task()
