944891f132b1dc65a8a059d9ebd099df07e29e2d
TereDom <TereDOmGames@gmail.com>
2020-06-21 19:29:59 +0300

    Сделана форма авторизации

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
-from flask import Flask, Blueprint, jsonify
+from flask import Blueprint, jsonify
 from flask_restful import Api, Resource, reqparse
 from data import db_session
 from data.__all_models import *
+from app import app
 
 
 db_session.global_init('db/DataBase.sqlite')
-app = Flask(__name__)
 api = Api(app)
 blueprint = Blueprint('api', __name__,
                       template_folder='templates')
---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, url_for, redirect
+from flask_login import LoginManager, login_user
+from data import db_session
+
+from api import *
+
+from data.__all_models import *
+from data.__all_forms import *
+
+
+db_session.global_init('db/DataBase.sqlite')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'elttaBhtaM'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main():
+    app.run(host='127.0.0.1', port=5000)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+
+@app.route('/')
+@app.route('/index')
+@app.route('/main')
+def index():
+    return '<a href="/sign_in">sign_in</a>'
+
+
+@app.route('/sign_in', methods=['GET', 'POST'])
+def sign_in():
+    form = LoginForm()
+    param = dict()
+    param['template_name_or_list'] = 'sign_in.html'
+    param['title'] = 'Авторизация'
+    param['form'] = form
+    param['base_style_way'] = url_for('static', filename='css/style.css')
+    param['style_way'] = url_for('static', filename='css/sign_in.css')
+    param['message'] = ''
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.login == form.login.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        param['message'] = "Неправильный логин или пароль"
+        return render_template(**param)
+    return render_template(**param)
+
+
+if __name__ == '__main__':
+    # app.register_blueprint(api.blueprint)
+    main()
