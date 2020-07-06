4221239e6b5052ca5000f8c72f7da3f6771c673f
Димка <mityka2004@yandex.ru>
2020-07-06 16:28:52 +0300

    Изменен файл requirements

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ login_manager.init_app(app)
 
 
 def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    app.run(host='127.0.0.1', port=5000)
 
 
 @login_manager.user_loader
