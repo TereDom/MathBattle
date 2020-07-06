795fd37d1177729a94fe7c4a25fe464a374bd443
Димка <mityka2004@yandex.ru>
2020-07-06 17:37:24 +0300

    Оставлено только api

--- api.py
+++ api.py
@@ -1,7 +1,23 @@
+from flask import Flask
 import tasks_resource, users_resource
 from flask_restful import Api
-from app import app
+import os
+from data import db_session
 
+
+db_session.global_init('db/DataBase.sqlite')
+app = Flask(__name__)
 api = Api(app)
 
 
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+session = db_session.create_session()
+api.add_resource(users_resource.UserResource, '/api/users/<user_login>')
+api.add_resource(users_resource.UserListResource, '/api/user/')
+api.add_resource(tasks_resource.TaskResource, '/api/task/<task_id>')
+api.add_resource(tasks_resource.TaskListResource, '/api/tasks/<user_login>')
+main()
--- developer_client.py
+++
@@ -1,6 +0,0 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QMessageBox
-
-from requests import get, post, put, delete
-
-
